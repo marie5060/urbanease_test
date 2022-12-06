6b7d46d187f6c46821232d82bdb83dee4cc14633
marie5060 <marie5060@hotmail.com>
2022-12-06 19:32:25 +0100

    boat s coords and tile 's type  added  on the map

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -7,13 +7,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Tile;
 use App\Repository\BoatRepository;
+use App\Repository\TileRepository;
 
 class MapController extends AbstractController
 {
     /**
      * @Route("/map", name="map")
      */
-    public function displayMap(BoatRepository $boatRepository) :Response
+    public function displayMap(BoatRepository $boatRepository, TileRepository $tileRepository) :Response
     {
         $em = $this->getDoctrine()->getManager();
         $tiles = $em->getRepository(Tile::class)->findAll();
@@ -22,11 +23,18 @@ class MapController extends AbstractController
             $map[$tile->getCoordX()][$tile->getCoordY()] = $tile;
         }
 
-        $boat = $boatRepository->findOneBy([]);
+           // get coord of boat position
+           $boat = $boatRepository->findOneBy([]);
+           $boatX=$boat->getCoordX();
+           $boatY=$boat->getCoordY();
+   
+           // get tile where is the boat
+           $tile = $tileRepository->findOneBy(['coordX'=>$boatX,'coordY'=> $boatY]);
 
         return $this->render('map/index.html.twig', [
             'map'  => $map ?? [],
             'boat' => $boat,
+            'tile' => $tile,
         ]);
     }
 }
